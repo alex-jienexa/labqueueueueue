820e2bd4b62be44a4a6c916791ebf1de81d58e79
mrMX <emirshuman1999@gmail.com>
2025-06-28 18:40:51 +0300

    Создание папки с api.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import {BrowserRouter, Navigate, Route, Routes, Router, useLocation, useNavigate} from "react-router-dom";
 import './App.css';
+
 import Login from "./pages/Login"
 import Home from "./pages/Home"
 import Queue from "./pages/Queue"
 import Create from "./pages/Create"
+import { useEffect } from "react";
 
 export const ROUTES = {
     LOGIN: "/login",
@@ -12,22 +14,42 @@ export const ROUTES = {
     CREATE: "/qcreate"
 }
 
+function checkObj(obj, check_val){
+  return Object.values(obj).includes(check_val)
+}
 
-function App() {
+
+function AppContext() {
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!checkObj(ROUTES, location.pathname)) {
+      navigate(-1);
+    }
+  });
+
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <div>
-          <Routes>
-            <Route exec path={ROUTES.LOGIN} element={<Login />} />
-            <Route path={ROUTES.HOME} element={<Home />} />
-            <Route path={ROUTES.QINFO} element={<Queue />} />
-            <Route path={ROUTES.CREATE} element={<Create />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <div>
+        <Routes>
+          <Route exec path={ROUTES.LOGIN} element={<Login />} />
+          <Route path={ROUTES.HOME} element={<Home />} />
+          <Route path={ROUTES.QINFO} element={<Queue />} />
+          <Route path={ROUTES.CREATE} element={<Create />} />
+        </Routes>
+      </div>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContext />
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/api/api.js
@@ -0,0 +1,11 @@
+
+const BASE_API_URL = "http://localhost:8080";
+import axios from 'axios'; 
+
+const API_METHODS = 
+{
+    GET: "GET",
+    POST: "POST",
+    PUT: "PUT",
+    DELETE: "DELETE",
+}

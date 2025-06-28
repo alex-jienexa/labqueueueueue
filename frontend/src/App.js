0937cebd96732a487c83583e2876f9852f716146
mrMX <emirshuman1999@gmail.com>
2025-06-28 17:17:56 +0300

    Скелет страниц и возможность ручного перехода. Обновление favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
+import {BrowserRouter, Navigate, Route, Routes, Router, useLocation, useNavigate} from "react-router-dom";
 import './App.css';
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import Queue from "./pages/Queue"
+import Create from "./pages/Create"
+
+export const ROUTES = {
+    LOGIN: "/login",
+    HOME: "/home",
+    QINFO: "/queue",
+    CREATE: "/qcreate"
+}
+
 
 function App() {
+
   return (
     <div className="App">
-      <div className='object'>Hello World</div>
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route exec path={ROUTES.LOGIN} element={<Login />} />
+            <Route path={ROUTES.HOME} element={<Home />} />
+            <Route path={ROUTES.QINFO} element={<Queue />} />
+            <Route path={ROUTES.CREATE} element={<Create />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/pages/Queue.js
@@ -0,0 +1,7 @@
+const Queue = () => {
+    return (
+        <div>Queue page</div>
+    );
+};
+
+export default Queue;
